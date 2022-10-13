63e7b06abd9a5f081fa8e0d5b5e38b34471fdf39
Abi0l9 <cryptog3ms@gmail.com>
2022-10-13 22:32:19 +0000

    6th task successful

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - returns the sizes of the inputs
+ * Return:  returns 0 for success
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+}
