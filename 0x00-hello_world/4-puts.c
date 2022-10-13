ec5e9d69032a7e3332bcef1fcd87fe20f446e590
Abi0l9 <cryptog3ms@gmail.com>
2022-10-13 04:32:27 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/*
+ * The function prints a string as output and 
+ * also return 0.
+ *
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+
+	return (0);
 }
