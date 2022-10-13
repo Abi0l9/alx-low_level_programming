4ac34ac88d752c24d470d2a859cc98dcc5a927e4
Abi0l9 <cryptog3ms@gmail.com>
2022-10-13 22:08:35 +0000

    5th task completed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this function prints a string.
+ * Return: return 0 if the code runs successfully,
+ * 1, otherwise.
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
