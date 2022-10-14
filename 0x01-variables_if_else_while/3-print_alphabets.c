ceb6a106198b5fe0e5d53919c99f4eb9c5bf2ca6
Abi0l9 <cryptog3ms@gmail.com>
2022-10-14 12:12:07 +0000

    fix

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *  main - prints the alphabets from a to z in lowercase,
+ *  and then in uppercase, followed by a new line.
+ *  Return: zero shoild be returned to indicate a successful compolation
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		int lower = tolower(x);
+		putchar(lower);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
