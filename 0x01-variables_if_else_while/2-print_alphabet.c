2e21dda37f3534fed7221d103fa8f5d89c10fd7b
Abi0l9 <cryptog3ms@gmail.com>
2022-10-14 12:02:04 +0000

    3rd task done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *  main - prints the alphabets from a to z in lowercase
+ *  Return: zero shoild be returned to indicate a successful compolation
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		int lower = tolower(x);
+		putchar(lower);
+	}
+	putchar('\n');
+	return (0);
+}
