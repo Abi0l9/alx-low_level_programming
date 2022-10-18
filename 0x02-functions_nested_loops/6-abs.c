a0ddc85fb57b332e612917fc8201a09d636834ed
Abi0l9 <cryptog3ms@gmail.com>
2022-10-18 21:48:19 +0000

    7th task done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  _abs - changes values to absolute
+ *  Return: returns (1)
+ *  @c: absolute parameter
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+		c = c * -1;
+	return (1);
+}
