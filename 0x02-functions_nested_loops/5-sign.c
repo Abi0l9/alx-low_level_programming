083fba375bd9894ee20c0b0c6c788095b1fd07bc
Abi0l9 <cryptog3ms@gmail.com>
2022-10-18 21:14:55 +0000

    6th task done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - prints a sign
+ * Return: returns a positive number or negative number or zero
+ * @c: the parameter passed to the function
+ */
+int print_sign(int c)
+{
+	int result;
+
+	if (c > 0)
+	{
+		_putchar('+');
+		result = 1;
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		result = 0;
+	}
+	else if (c < 0)
+	{
+		_putchar('-');
+		result = -1;
+	}
+	return result;
+}
