e7a51d14c8e6414195549dc200db8730785f3e2e
Abi0l9 <cryptog3ms@gmail.com>
2022-10-18 11:43:56 +0000

    3rd task done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,19 @@
 #include "main.h"
-/**
- * main - check the code.
- * Return: Always 0.
- */
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j = 'a';
+		int l = 'z';
+
+		while (j <= l)
+		{
+			_putchar(j);
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
 }
