2d87e915739a06fafe2fcde9c58a5cf7e806cb8d
Abi0l9 <cryptog3ms@gmail.com>
2022-10-18 22:45:31 +0000

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,12 @@
 /**
  *print_last_digit- prints the last digit of a number to the output
  * Return: returns an int
- * @c: passed parameter
+ * @n: passed parameter
  */
 int print_last_digit(int n)
 {
 	if (n < 0)
-		n *= -1;
+		n = n * (-1);
 	_putchar((n % 10) + '0');
 	return (n % 10);
 }
